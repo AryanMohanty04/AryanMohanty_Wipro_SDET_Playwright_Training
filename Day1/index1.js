c9719326c34ef16cd35d53d0ed45b8cc034de6a8
TadaKakehashi <aryan333498@gmail.com>
2026-04-30 16:04:22 +0530

    Arrays, Loops and Conditional Statement Demonstration

--- Day1/index1.js
+++ Day1/index1.js
@@ -62,3 +62,71 @@ console.log(v)
 console.log("Outside of Scope");
 console.log(l);
 console.log(v);
+
+
+console.log("----------------------------")
+console.log("Demonstrating Arrays and Loops implementation: ")
+let arr = [1,"Aryan",true, 6.8];
+let arr2 = [1,2,3,4,5,6];
+
+console.log("Demonstrating for loop implementation: ")
+for(let i=0;i<arr.length;i++){
+    console.log("Element at index "+i+ ": " +arr[i]);
+}
+
+console.log()
+console.log("Demonstrating while loop implementation: ")
+let i = 0;
+while(i<arr2.length){
+    console.log("Element at index "+i+ ": " +arr2[i]);
+    i++;
+}
+
+console.log()
+console.log("Demonstrating for-each loop: ")
+const numbers = [1,2,3,4];
+numbers.forEach(function(num){
+    console.log(num);
+});
+
+
+console.log("----------------------------")
+console.log("Conditional Statement Demonstration: ");
+let num1 = 100;
+let num2 = 10;
+
+if(num1 > num2){
+    console.log("i is greater");
+} else {
+    console.log("j is greater");
+}
+
+console.log()
+console.log("Switch Case Demonstration: ")
+
+let choice = 1;
+switch(choice){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid choice (select between 1-7)");
+}
