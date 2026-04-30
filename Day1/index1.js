43c18000858ee51a82f9a043c5ecc8fffcd59a97
TadaKakehashi <aryan333498@gmail.com>
2026-04-30 12:00:48 +0530

    String Operations, Mathematical Operations, Logical Operations

---
+++ Day1/index1.js
@@ -0,0 +1,32 @@
+let a = 5;
+let b = 10;
+let str = "Hello, World!";
+
+// String operations
+console.log("String: ", str);
+console.log("Length of String: ",str.length);
+console.log("Uppercase: ",str.toUpperCase());
+console.log("Lowercase: ",str.toLowerCase());
+console.log("Substring: ",str.substring(0,5));
+console.log("----------------------------")
+
+//Mathematical Operations
+console.log("Addition: ",a+b);
+console.log("Substraction: ",a-b);
+console.log("Multiplication: ",a*b);
+console.log("Division: ",a/b);
+console.log("Remainder: ",a%b);
+
+console.log("----------------------------")
+
+//Logical operations
+a = 100;
+b = 200;
+
+console.log("a > b:",a>b);
+console.log("a < b:",a<b);
+console.log("a == b: ",a==b);
+console.log("a === b: ",a===b);
+console.log("a != b:", a != b);
+console.log("a >= b:", a>=b);
+console.log("a <= b:", a<=b);
