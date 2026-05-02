fd6ac917218f5416d2bb77b2408048e9fd2632c8
TadaKakehashi <aryan333498@gmail.com>
2026-05-02 09:13:38 +0530

    Initializing Day1 files

---
+++ Day2/index2.js
@@ -0,0 +1,14 @@
+let a = "Aryan";
+let b = "Tada";
+let c = "Ray";
+
+let d = "Hello "+ a + ", " +b+" and "+c+"!";
+console.log(d);
+
+
+// Template Literals
+let e = `Hello ${a}, ${b} and ${c}!`;
+console.log(e);
+
+let f = `The length of a is ${a.length}`;
+console.log(f);
