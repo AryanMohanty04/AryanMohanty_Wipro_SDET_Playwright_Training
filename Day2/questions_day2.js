a96a46cae75b0cdb6b9336282a369030830ae9ca
TadaKakehashi <aryan333498@gmail.com>
2026-05-02 14:51:25 +0530

    questions day 2 solutions

---
+++ Day2/questions_day2.js
@@ -0,0 +1,56 @@
+/*Q1>Practice Template Literals and Array Methods.
+Write a function called formatGuests that takes an array of names. The function should:
+Remove the first name from the list (it’s the host, not a guest).
+Add "Guest: " before each remaining name using .map().
+Return a single string where each guest is on a new line.*/
+
+function formatGuests(names) {
+    let guests = names.slice(1);
+    let formatted = guests.map(name => `Guest: ${name}`);
+
+    let result = "";
+
+    for (let i = 0; i < formatted.length; i++) {
+        result += formatted[i];
+        if (i !== formatted.length - 1) {
+            result += "\n";
+        }
+    }
+
+    return result;
+}
+console.log(formatGuests(["Aryan", "Rahul", "Neha", "Priya"]));
+
+/*Question 2: The Logic Gate (Filter & Math)
+Goal: Practice Arrow Functions, filter, and Math methods.
+Create an arrow function called getHighNumbers. It should take an array of mixed types (strings and numbers) and:
+Filter the array to keep only the numbers.
+Filter those numbers again to only keep ones greater than a random threshold between 1 and 50.
+Return the final array.*/
+
+const getHighNumbers = (arr) => {
+    const threshold = Math.floor(Math.random() * 50) + 1;
+    console.log("Threshold:", threshold);
+    return arr
+        .filter(x => typeof x === "number")   
+        .filter(x => x > threshold);          
+};
+
+console.log(getHighNumbers([10, "Aryan", 25, 60, "hello", 5, 42]));
+
+// Question 3: The Price Calculator Goal: Practice Type Conversion and splice. You have an array of prices, but they are currently strings. Write a script that: Removes the last item in the array using an array method (because it's a "handling fee" you want to calculate separately). Converts the remaining string prices into actual numbers. Calculates the sum of those numbers using an arrow function. Prints: "Total Price: $[sum]" using a template literal.
+
+const sum = (arr) => {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+
+    return total;
+};
+
+let prices = ["100", "200", "300", "50"];
+prices.splice(-1, 1);
+let numericPrices = prices.map(Number);
+let total = sum(numericPrices);
+console.log(`Total Price: $${total}`);
