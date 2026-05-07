b3eeb516b2f9e05ddcc8d7fa646e43aa2f46eb3f
Aryan Mohanty <aryan333498@gmail.com>
2026-05-07 14:39:26 +0530

    Prototype

--- Day6/prototype1.js
+++ Day6/prototype1.js
@@ -1,17 +1,36 @@
-function Person(name) {
-    this.name = name;
+// function Person(name) {
+//     this.name = name;
+// }
+
+// Person.prototype.sayHi = function () {
+//     console.log("hello " + this.name);
+// };
+
+// const me = new Person("aryan");
+// const you = new Person("you");
+
+// me.sayHi();
+// you.sayHi();
+
+// console.log(Object.getPrototypeOf(me) === Person.prototype);
+
+// console.log(me.sayHi === you.sayHi);
+
+function Car(brand, speed) {
+
+    this.brand = brand;
+    this.speed = speed;
 }
-
-Person.prototype.sayHi = function () {
-    console.log("hello " + this.name);
+Car.prototype.drive = function () {
+    console.log(this.brand + " is driving");
 };
+Car.prototype.brake = function () {
+    console.log(this.brand + " stopped");
+};
+const bmw = new Car("BMW", 100);
+const audi = new Car("Audi", 180);
 
-const me = new Person("aryan");
-const you = new Person("you");
+bmw.drive();
+audi.drive();
 
-me.sayHi();
-you.sayHi();
-
-console.log(Object.getPrototypeOf(me) === Person.prototype);
-
-console.log(me.sayHi === you.sayHi);
+bmw.brake();
