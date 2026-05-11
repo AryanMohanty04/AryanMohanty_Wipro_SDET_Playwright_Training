c731e84cfb411369eaa257f550177f1179245810
Aryan Mohanty <aryan333498@gmail.com>
2026-05-11 12:38:39 +0530

    TypeScript implementation

--- Day9/typescript/app.ts
+++ Day9/typescript/app.ts
@@ -1,2 +1,12 @@
 let message: string = "Hello Aryan";
-console.log(message);
+let result: number | string;
+result = 45;
+result = "Success";
+console.log(message, result);
+
+
+function add(a:number,b:number):number{
+    return a + b;
+}
+console.log(add(4,5));
+
